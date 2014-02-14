e99e48a85457494762be69d5ae85bccf4d1feeaf
Dmitriy Mosnenko <Dima.Mosnenko@adstream.com>
2014-02-14 13:20:48 +0200

    Initial commit Added Readme and placed scripts

--- README.md
+++ README.md
@@ -2,3 +2,56 @@ ejabberd2openfire
 =================
 
 Users migration Python script
+
+
+A short usage example
+
+    On your server running EJabberD, create a dumpfile using:
+
+ejabberdctl dump ejabberd_dumpfile
+
+ - for this ejabberd should be started from root user (check init scipt)
+
+    BEWARE! The file will turn up in your database path, most probably in '''/var/lib/ejabberd/ejabberd_dumpfile'''.
+    Use ejabberd2openfire.py to create a OpenFire import/export XML-File:
+
+cd your_ejabberd2openfire_directory/ && ./ejabberd2openfire.py ejabberd_dump new_import_data.xml
+
+    Now, surf to your OpenFires web-admin-interface, on the user import/export page, e.g.:d
+
+http://YOUR.DOMA.IN:9090/plugins/userimportexport/import-user-data.jsp
+
+ - for this should be installed plugin "User Import Export"
+
+    Import the data. Hopefully everything works. Now enjoy your new OpenFire Installation using your old EJabberd data!
+
+ Source: 
+ http://indefero.ghostdub.de/index.php/p/ejabberd2openfire/page/Usage/
+
+
+ In my case I haven't users in Ejabberd at all (authorization was via another way),
+ but that were presents rosters and vCard, I not needed in vCard.
+
+ And this script did not want to do migration only of rosters.
+ Therefore script was changed for ability create users from the rosters
+ and then assign rosters to their users.
+
+ Usage is identical for previous one, but only use script:
+
+./roster_ejabberd2openfire.py ejabberd_dump new_import_data.xml
+
+Also this script sets empty password, because in roster password not specified,
+you may change it in line
+
+    <Roster>"""%(user[0].replace("\\\\","\\"),"YOUR_PASSWORD_FOR_ALL_USERS",int(time.time()), int(time.time())))
+
+Besides was improved migration of user ID, example:
+userID, how it see human:
+	miami@test.com
+userID, how it is in ejabberd dump:
+	miami\\test.com
+userID, how it should be in xml for import to Openfire:
+	miami\40test.com
+
+Therefore I do this replace:
+	...user[0].replace("\\\\","\\")...
---
+++ roster_ejabberd2openfire.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import sys,pprint,time
+
+if len(sys.argv) != 3:
+    print "Usage: ejabberd2openfire.py <ejabberd dumpfile> <openfire import/export XML file> -- where the openfire xml file will be created // overwritten"
+    sys.exit()
+
+ejdump=open(sys.argv[1], "r").read()
+ofirexml=open(sys.argv[2], "w")
+
+substdict = {'remove':'-1',
+             'none':'0',
+             'to':'1',
+             'from':'2',
+             'both':'3'}
+recvstdict = {'none':'-1',
+              'subscribe':'1',
+              'unsubscribe':'2'
+              }
+askstdict = {'in':'1',
+             'out':'0',
+             'none':'-1'
+             }
+
+userpws = {}
+rosters = {}
+
+fromidx=0
+while ejdump.find("{roster", fromidx) != -1:
+    nidx=ejdump.find("{roster", fromidx)
+    eidx=ejdump.find(",\n",nidx)
+    finstr=ejdump[nidx:eidx+2]
+    if "record_name" in finstr:
+        fromidx=eidx
+    else:
+        pprint.pprint( finstr.replace("{","").replace("}","").replace('"',"").replace("\n","").split(",")[1:3] )
+        uid, domain = finstr.replace("{","").replace("}","").replace('"',"").replace("\n","").split(",")[1:3]
+        userpws[(uid,domain)] = uid[0:-1]
+        fromidx=eidx
+
+fromidx=0
+while ejdump.find("{roster", fromidx) != -1:
+    nidx=ejdump.find("{roster", fromidx)
+    eidx=ejdump.find("}.",nidx)
+    finstr=ejdump[nidx:eidx+2].replace("\n","")
+    if "record_name" in finstr or "roster_groups" in finstr:
+        fromidx=eidx
+    else:
+        fina = []
+        for i in finstr.replace("{","").replace("}","").replace('"',"").split(","):
+            fina.append(i.strip())
+        fromusr = tuple(fina[1:3])
+        tousr = tuple(fina[3:5])
+        subst = fina[12]
+        recvst = fina[13]
+        askst = fina[13]
+        
+        if not subst in substdict.keys():
+            subst = 'none'
+        if not recvst in recvstdict.keys():
+            recvst = 'none'
+        if not askst in askstdict.keys():
+            askst = 'none'
+        
+        group = None
+        if '[' in fina[14] and ']' in fina[14] and '[]' not in fina[14]:
+            group = fina[14].strip().strip( '[]' )
+        name = ""
+        if fina[11] != "[]":
+            name = fina[11]
+        fromidx=eidx
+        if not fromusr in rosters:
+            rosters[fromusr]=[]
+        if tousr[0]=='[]':
+            tousr=('',tousr[1])
+        name=name.replace("<","").replace(">","") # TODO: replace with correctly escaped fun
+        rosters[fromusr].append( (tousr,name,subst,askst,recvst,group) )
+
+
+ofirexml.write('<?xml version="1.0" encoding="UTF-8"?>\n<Openfire>\n\n')
+
+# pprint.pprint( userpws.keys() )
+# pprint.pprint( rosters.keys() )
+for user,pw in userpws.iteritems():
+    ofirexml.write("""
+  <User>
+    <Username>%s</Username>
+    <Email></Email>
+    <Name></Name>
+    <Password>%s</Password>
+    <CreationDate>%s</CreationDate>
+    <ModifiedDate>%s</ModifiedDate>
+    <Roster>"""%(user[0].replace("\\\\","\\"),"",int(time.time()), int(time.time())))
+    
+    if user in rosters.keys():
+        for i in rosters[user]:
+            subst = substdict[ i[2] ]
+            askst = askstdict[ i[3] ]
+            recvst = recvstdict[ i[4] ]
+            jid = "@".join(i[0]).strip("@")
+            if i[5] != None:
+                ofirexml.write("""
+      <Item jid="%s" askstatus="%s" recvstatus="%s" substatus="%s" name="%s" >
+        <Group>%s</Group>
+      </Item>"""%( jid, askst, recvst, subst, i[1], i[5] ))
+            else:
+                ofirexml.write("""
+      <Item jid="%s" askstatus="%s" recvstatus="%s" substatus="%s" name="%s" />"""%( jid.replace("\\\\","\\"), askst, recvst, subst, i[1]))
+                
+    ofirexml.write("""
+    </Roster>""")
+    
+    ofirexml.write("""
+  </User>""")
+    
+ofirexml.write('\n</Openfire>')
+ofirexml.close()
